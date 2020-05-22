cda95207cd5f32a1657eff548a8b8a734d6ed85d
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2020-05-22 14:10:04 -0300

    Se arranca a trabajar sobre utils

---
+++ utils/driver.wrapper.js
@@ -0,0 +1,23 @@
+const UtilsWrapper = ({
+
+  closeComponent(closeButtonSelector, componentSelector) {
+    const closeButton = $(closeButtonSelector);
+    const component = $(componentSelector);
+    //aca capaz mejor un es visible
+    while(component.isDisplayedInViewport()){
+      browser.pause(1000);
+      if (closeButton.isClickable()) closeButton.click();
+    }
+  },
+
+  closeCovidModal() {
+    this.closeComponent('.modal-dialog .close-modal','.modal-content')
+  },
+
+  closeCookiesBanner() {
+    this.closeComponent('.cc-btn','.cc-banner')
+  }
+
+});
+
+module.exports = UtilsWrapper
