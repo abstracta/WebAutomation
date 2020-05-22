5eb729a0cf10e3ba8c8174ac1cfe6500dbb9ba6d
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2020-05-22 14:16:32 -0300

    Se arranca a trabajar en page object de resources

---
+++ pages/resources.js
@@ -0,0 +1,12 @@
+const ResourcesPage = Object.create({
+  /**
+  * define elements
+  */
+  title: { get() { return browser.getTitle() } },
+  /**
+  * define or overwrite page methods
+  */
+
+});
+
+module.exports = ResourcesPage;
