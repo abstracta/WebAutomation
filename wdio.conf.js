3c2802b8dd4c42ff819dbc4f6454334c3b44ac19
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2020-05-22 10:36:33 -0300

    Se agrega config para ejecucion remota de pruebas

--- wdio.conf.js
+++ wdio.conf.js
@@ -50,9 +50,15 @@ exports.config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 5,
+        maxInstances: 1,
         //
         browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: [
+                '--no-sandbox',
+                '--headless', 
+                '--window-size=1366,720']
+        }
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
