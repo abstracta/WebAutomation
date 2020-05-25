4f4ef3a963cbe4a03d6aecdb032e4d58e52fd057
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2020-05-25 09:51:06 -0300

    Se sigue avanzando con resources

--- pages/resources.js
+++ pages/resources.js
@@ -3,11 +3,32 @@ const ResourcesPage = Object.create({
   * define elements
   */
   title: { get() { return browser.getTitle(); } },
+  pageQuantity: { get() { return $$('//ul[@id="page-list"]/li').length - 2; } },
+  cards: { get() { return $$('.resources-container .resource-container[style=""] .resource'); } },
+  nextPageButton: { get() { return $('.next-arrow'); } },
   /**
   * define or overwrite page methods
   */
   open() {
       browser.url(`${browser.options.baseUrl}/insights/resources`);
+  },
+  checkTypeFilter(type) {
+    let retorno = true;
+    
+    let filterButton = $(`//div[contains(@class, "categories-container")]/div[@data-type="${type}"]`);
+    filterButton.click();
+    
+    for (let index = 0; index < this.pageQuantity; index++) {
+      
+      this.cards.forEach(card => {
+          if(! card.getText().includes(`${type}`)) retorno = false;
+      });
+
+      if(ResourcesPage.pageQuantity > index + 1) ResourcesPage.nextPageButton.click();
+    
+    }
+
+    return retorno;
   }
 });
 
--- specs/resources.js
+++ specs/resources.js
@@ -1,12 +1,21 @@
 const ResourcesPage = require('../pages/resources');
+const UtilsWrapper = require('../utils/driver.wrapper');
 const assert = require('assert');
 
 describe('Abstracta US resources page', () => {
     before(() => {
         ResourcesPage.open();
+        UtilsWrapper.closeCovidModal();
+        UtilsWrapper.closeCookiesBanner();
     })
 
-    it('Should have the right title', () => {
+    it('should have the right title', () => {
         assert.strictEqual(ResourcesPage.title.get(), 'Software Testing Resources and Guides - Abstracta');
+    }),
+    it('brochure type filter should work properly', () =>{
+        let types = ['BROCHURE','CASE-STUDY','EBOOK','INFOGRAPHIC','QUIZZES','WEBINAR','WHITE-PAPER'];
+        types.forEach(type => {
+            assert.strictEqual(ResourcesPage.checkTypeFilter(`${type}`), true)    
+        });
     })
 })
